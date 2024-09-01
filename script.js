551dff2a3012b025fc879e40cfffa6e2bf16fafa
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-09-01 22:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let student = {
+	name:"prince"
+	
+};
+object.prototype.getkeys=function(){
+	return object.keys;
+};
+let keys = student.getkeys();
+console.log(keys);
